5700b8aa3f4a46491c146d4d2bcd7974d1197c7b
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-11-03 18:19:07 +0900

    build : 프로젝트 생성

---
+++ SungMinCho/SungMinCho/SungMinChoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SungMinChoApp.swift
+//  SungMinCho
+//
+//  Created by 조성민 on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SungMinChoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SungMinCho/SungMinChoUITests/SungMinChoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SungMinChoUITestsLaunchTests.swift
+//  SungMinChoUITests
+//
+//  Created by 조성민 on 11/3/24.
+//
+
+import XCTest
+
+final class SungMinChoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
