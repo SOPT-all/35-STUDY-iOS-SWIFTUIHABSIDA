7b0209e3be3261223032e77d19e314d5f774a934
sem-git <lsmsm0327@naver.com>
2024-11-03 22:43:13 +0900

    init: 프로젝트 생성

---
+++ LeeSeminSU/LeeSeminSU/LeeSeminSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeeSeminSUApp.swift
+//  LeeSeminSU
+//
+//  Created by 이세민 on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LeeSeminSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
