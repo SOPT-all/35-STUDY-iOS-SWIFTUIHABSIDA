439bb9c6da97f582c4f52ac9197e0e85a56d2dc5
sangwook <operawook@gmail.com>
2024-11-03 23:07:18 +0900

    [feat] init project

---
+++ sangwook/sangwook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sangwook
+//
+//  Created by 우상욱 on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sangwook/sangwook/sangwookApp.swift
@@ -0,0 +1,17 @@
+//
+//  sangwookApp.swift
+//  sangwook
+//
+//  Created by 우상욱 on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct sangwookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
