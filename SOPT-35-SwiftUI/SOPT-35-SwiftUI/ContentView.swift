2582ded9d72ab91adc5b612b1ce047034482190c
cirtuare <cirtuare@gmail.com>
2024-11-03 23:52:25 +0900

    [Setting] 프로젝트 생성

---
+++ SOPT-35-SwiftUI/SOPT-35-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SOPT-35-SwiftUI
+//
+//  Created by 이수민 on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SOPT-35-SwiftUI/SOPT-35-SwiftUI/SOPT_35_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOPT_35_SwiftUIApp.swift
+//  SOPT-35-SwiftUI
+//
+//  Created by 이수민 on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SOPT_35_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
