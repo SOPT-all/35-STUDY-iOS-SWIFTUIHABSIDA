b5184ee22784b0aab281c777b0150d4ee9cf812a
0gonge <Challengef0802@gmail.com>
2024-11-03 19:31:07 +0900

    week1: 구조 살펴보기

---
+++ songyeogyeongSU/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  songyeogyeongSU
+//
+//  Created by 송여경 on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ songyeogyeongSU/songyeogyeongSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  songyeogyeongSUApp.swift
+//  songyeogyeongSU
+//
+//  Created by 송여경 on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct songyeogyeongSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
