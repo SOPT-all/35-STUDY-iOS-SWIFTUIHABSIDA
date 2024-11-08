7e325340d72435da79a3fbb74ca2ae06790d1251
won <andrew112517@naver.com>
2024-11-08 17:14:41 +0900

    [Build] 프로젝트 생성

---
+++ SeungWonSU/SeungWonSU/SeungWonSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeungWonSUApp.swift
+//  SeungWonSU
+//
+//  Created by 김승원 on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SeungWonSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
