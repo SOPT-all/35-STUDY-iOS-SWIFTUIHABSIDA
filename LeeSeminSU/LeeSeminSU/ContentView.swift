00b0e875746d2fd58ea2a5045da4b00ce63f2cfc
sem-git <lsmsm0327@naver.com>
2024-11-11 01:22:37 +0900

    feat: UIComponent 실습

--- LeeSeminSU/LeeSeminSU/ContentView.swift
+++ LeeSeminSU/LeeSeminSU/ContentView.swift
@@ -11,7 +11,9 @@ struct ContentView: View {
     var body: some View {
         
 //        VStackHStackExample()
-        ZStackExampleView()
+//        ZStackExampleView()
+        
+        ComponentExample()
     }
 }
 
