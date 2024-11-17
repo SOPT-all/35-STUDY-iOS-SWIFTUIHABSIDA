96b46b32bccf457368d260750aff4e1b24f338b5
won <andrew112517@naver.com>
2024-11-17 18:56:40 +0900

    [Feat] 3주차 실습

--- SeungWonSU/SeungWonSU/week3View.swift
+++ SeungWonSU/SeungWonSU/week3View.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct week3View: View {
+    let data = Array(1...30).map { "Minji \($0)"}
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    let rows = [
+        GridItem(.fixed(100)),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            ScrollView {
+                ScrollView(.horizontal) {
+                    LazyHGrid(rows: rows, spacing: 10) {
+                        ForEach(data, id: \.self) { i in
+                            VStack(spacing: 0) {
+                                Image(.minji)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .cornerRadius(15)
+                                    .frame(width: 100, height: 100)
+                                Text(i)
+                            }
+                        }
+                    }
+                }
+
+                
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(data, id: \.self) { i in
+                        VStack(spacing: 10) {
+                            Image(.minji)
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(15)
+                            Text(i)
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .scrollIndicators(.hidden)
+        }
     }
+    
+    
 }
 
 #Preview {
