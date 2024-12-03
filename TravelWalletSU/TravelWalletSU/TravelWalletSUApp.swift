09489ea598c2d0c29e45bae2b95a025cbd6c0f09
won <andrew112517@naver.com>
2024-12-04 02:37:12 +0900

    [Setting] 프로젝트 파일 생성

---
+++ TravelWalletSU/TravelWalletSU/TravelWalletSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelWalletSUApp.swift
+//  TravelWalletSU
+//
+//  Created by 김승원 on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelWalletSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
