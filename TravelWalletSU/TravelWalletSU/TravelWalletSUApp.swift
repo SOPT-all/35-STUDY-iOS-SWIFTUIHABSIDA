ec0b249ff612954f6686d0ddb786d61d13b7fd2f
won <andrew112517@naver.com>
2024-12-03 16:27:31 +0900

    [Setting] 클론 코딩 프로젝트 생성

---
+++ TravelWalletSU/TravelWalletSU/TravelWalletSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelWalletSUApp.swift
+//  TravelWalletSU
+//
+//  Created by 김승원 on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelWalletSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
