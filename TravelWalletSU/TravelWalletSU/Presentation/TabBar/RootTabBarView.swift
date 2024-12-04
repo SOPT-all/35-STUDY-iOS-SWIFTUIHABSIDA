096b126a40939d5d395b5690d710fd88a037296f
won <andrew112517@naver.com>
2024-12-04 18:31:47 +0900

    [Feat] 커스텀 탭바 구현

--- TravelWalletSU/TravelWalletSU/Presentation/TabBar/RootTabBarView.swift
+++ TravelWalletSU/TravelWalletSU/Presentation/TabBar/RootTabBarView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct RootTabBarView: View {
     
-    enum Tab {
-        case pay, remittance, directPurchase, travel, myPage
+    enum Tab: String {
+        case pay = "페이"
+        case remittance = "송금"
+        case directPurchase = "직구"
+        case travel = "여행"
+        case myPage = "마이"
     }
     
     @State private var selected: Tab = .pay
@@ -52,51 +56,113 @@ struct RootTabBarView: View {
         }
     }
     
-    
     // MARK: - TabBar
+    
     var tabBar: some View {
         HStack {
+            TabBarButton(selected: $selected, tab: .pay)
             Spacer()
-            
+            TabBarButton(selected: $selected, tab: .remittance)
+            Spacer()
+            TabBarButton(selected: $selected, tab: .directPurchase)
+            Spacer()
+            TabBarButton(selected: $selected, tab: .travel)
+            Spacer()
+            TabBarButton(selected: $selected, tab: .myPage)
+        }
+        .padding(.horizontal, 29)
+        .padding(.top, 9)
+        .ignoresSafeArea()
+        .background(
+            Color(hex: "FFFFFF")
+                .ignoresSafeArea()
+                .shadow(color: .black.opacity(0.3), radius: 7, x: 0, y: 5)
+        )
+    }
+}
+
+struct TabBarButton: View {
+    
+    @Binding var selected: RootTabBarView.Tab
+    var tab: RootTabBarView.Tab
+
+    var body: some View {
+        switch tab {
+        case .pay:
             Button {
                 selected = .pay
             } label: {
-                Image(systemName: "globe")
+                VStack(spacing: 8) {
+                    Image(.payIcon)
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 18, height: 13)
+                    Text(tab.rawValue)
+                        .font(.system(size: 10, weight: .medium))
+                }
+                .foregroundStyle(selected == .pay ? Color(hex: "0BAEFF") : Color(hex: "BDBDBD"))
             }
             
-            Spacer()
-            
+        case .remittance:
             Button {
                 selected = .remittance
             } label: {
-                Image(systemName: "globe")
+                VStack(spacing: 5) {
+                    Image(.remittanceIcon)
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 19, height: 19)
+                    Text(tab.rawValue)
+                        .font(.system(size: 10, weight: .medium))
+                }
+                .foregroundStyle(selected == .remittance ? Color(hex: "0BAEFF") : Color(hex: "BDBDBD"))
             }
             
-            Spacer()
-            
+        case .directPurchase:
             Button {
                 selected = .directPurchase
             } label: {
-                Image(systemName: "globe")
+                VStack(spacing: 5) {
+                    Image(.directPurchaseIcon)
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 18, height: 18)
+                    Text(tab.rawValue)
+                        .font(.system(size: 10, weight: .medium))
+                }
+                .foregroundStyle(selected == .directPurchase ? Color(hex: "0BAEFF") : Color(hex: "BDBDBD"))
             }
             
-            Spacer()
-            
+        case .travel:
             Button {
                 selected = .travel
             } label: {
-                Image(systemName: "globe")
+                VStack(spacing: 5) {
+                    Image(.travelIcon)
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 18, height: 18)
+                    Text(tab.rawValue)
+                        .font(.system(size: 10, weight: .medium))
+                }
+                .foregroundStyle(selected == .travel ? Color(hex: "0BAEFF") : Color(hex: "BDBDBD"))
             }
             
-            Spacer()
-            
+        case .myPage:
             Button {
                 selected = .myPage
             } label: {
-                Image(systemName: "globe")
+                VStack(spacing: 5) {
+                    Image(.myPageIcon)
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 16, height: 17)
+                    Text(tab.rawValue)
+                        .font(.system(size: 10, weight: .medium))
+                }
+                .foregroundStyle(selected == .myPage ? Color(hex: "0BAEFF") : Color(hex: "BDBDBD"))
             }
             
-            Spacer()
         }
     }
 }
