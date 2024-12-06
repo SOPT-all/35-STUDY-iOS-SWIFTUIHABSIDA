773f867f0cac9319f307af46fc44be7f0eb7cef5
sem-git <lsmsm0327@naver.com>
2024-12-07 01:18:19 +0900

    feat: 상단 네비게이션바 UI 초기 구현

--- LeeSeminSU/LeeSeminSU/Week5 - TravelWallet/Main.swift
+++ LeeSeminSU/LeeSeminSU/Week5 - TravelWallet/Main.swift
@@ -10,10 +10,37 @@ import SwiftUI
 struct Main: View {
     var body: some View {
         TabView {
-            Text("페이")
-                .tabItem {
-                    Label("페이", image: "pay")
+            VStack {
+                HStack {
+                    Text("홈")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.trailing, 10)
+                    Text("소셜페이")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0.74, green: 0.74, blue: 0.74))
+                    
+                    Spacer()
+                    
+                    Image("qr")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                        .padding(.trailing, 10)
+                    Image("notification")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
                 }
+                .padding()
+                
+                Spacer()
+            }
+            
+            .tabItem {
+                Label("페이", image: "pay")
+            }
             
             Text("송금")
                 .tabItem {
