4b2c1c61d38267de4fe83eb232d6ede1bf32a895
sem-git <lsmsm0327@naver.com>
2024-12-08 03:11:22 +0900

    feat: 충전하기 버튼 클릭 시 충전 화면으로 전환하도록 구현

---
+++ LeeSeminSU/LeeSeminSU/Week5 - TravelWallet/Charge.swift
@@ -0,0 +1,18 @@
+//
+//  Charge.swift
+//  LeeSeminSU
+//
+//  Created by 이세민 on 12/8/24.
+//
+
+import SwiftUI
+
+struct Charge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Charge()
+}
--- LeeSeminSU/LeeSeminSU/Week5 - TravelWallet/Main.swift
+++ LeeSeminSU/LeeSeminSU/Week5 - TravelWallet/Main.swift
@@ -12,155 +12,157 @@ struct Main: View {
     
     var body: some View {
         TabView {
-            VStack {
-                // 상단 네비게이션바
-                HStack {
-                    Text("홈")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .padding(.trailing, 10)
-                    Text("소셜페이")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(red: 0.74, green: 0.74, blue: 0.74))
-                    
-                    Spacer()
-                    
-                    Image("qr")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                        .padding(.trailing, 10)
-                    Image("notification")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                }
-                .padding()
-                
-                // 플래티늄 카드
-                HStack {
-                    Image("platinumCard")
-                    VStack(alignment: .leading, spacing: 5){
-                        Text("드디어 나왔다!")
-                            .font(.system(size: 13))
-                            .foregroundColor(Color(hex: "#616161"))
-                        HStack(spacing: 0) {
-                            Text("트래블월렛 ")
-                                .font(.system(size: 16, weight: .bold))
-                                .foregroundColor(Color(hex: "#000000"))
-                            Text("플래티늄")
-                                .font(.system(size: 16, weight: .bold))
-                                .foregroundColor(Color(hex: "#0079FF"))
-                            Text(" 카드")
-                                .font(.system(size: 16, weight: .bold))
-                                .foregroundColor(Color(hex: "#000000"))
-                        }
-                    }
-                    Spacer()
-                    Image("arrow")
-                }
-                .padding()
-                .frame(width: 343, height: 76)
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(
-                            LinearGradient(
-                                gradient: Gradient(colors: [
-                                    Color(hex: "6FE8B7"),
-                                    Color(hex: "35DAE3")
-                                ]),
-                                startPoint: .leading,
-                                endPoint: .trailing
-                            )
-                        )
-                )
-                .padding(.bottom)
-                
-                // 내 지갑
-                HStack{
-                    VStack(spacing: 45) {
-                        HStack {
-                            Text("내 지갑")
-                                .font(.system(size: 18, weight: .semibold))
-                            Image("info")
-                            
-                            Spacer()
-                            
-                            Text("이용내역")
-                                .font(.system(size: 15, weight: .medium))
-                            Image("arrow")
-                                .padding(.leading, -13)
-                        }
-                        .padding(.horizontal, 5)
-                        
-                        Text("아직 충전된 외화가 없습니다.")
-                            .font(.system(size: 13))
-                            .foregroundColor(Color(hex: "#616161"))
-                        
-                        Button(action: {
-                        }) {
-                            Text("충전하기")
-                                .font(.system(size: 15, weight: .bold))
-                                .foregroundColor(.white)
-                                .frame(width: 302, height: 48)
-                                .background(Color(hex: "#0BAEFF"))
-                                .cornerRadius(12)
-                        }
-                    }
-                    .padding()
-                    .frame(width: 343, height: 228)
-                    .background(
-                        RoundedRectangle(cornerRadius: 15)
-                            .fill(Color(hex: "#FFFFFF"))
-                    )
-                }
-                .padding(.bottom)
-                
-                // 달러박스
-                if isDollarBoxVisible {
+            NavigationView {
+                VStack {
+                    // 상단 네비게이션바
                     HStack {
-                        HStack(spacing: 20) {
-                            Image("kakaoBank")
-                            
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text("카카오뱅크 이용중이시라면")
-                                    .font(.system(size: 10))
-                                Text("달러박스를 연동해보세요!")
-                                    .font(.system(size: 13, weight: .bold))
-                            }
-                        }
+                        Text("홈")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.trailing, 10)
+                        Text("소셜페이")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(red: 0.74, green: 0.74, blue: 0.74))
                         
                         Spacer()
                         
-                        Button(action: {
-                            isDollarBoxVisible = false
-                        }) {
-                            Image("xmark")
-                        }
+                        Image("qr")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                            .padding(.trailing, 10)
+                        Image("notification")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
                     }
                     .padding()
-                    .frame(width: 343, height: 66)
+                    
+                    // 플래티늄 카드
+                    HStack {
+                        Image("platinumCard")
+                        VStack(alignment: .leading, spacing: 5){
+                            Text("드디어 나왔다!")
+                                .font(.system(size: 13))
+                                .foregroundColor(Color(hex: "#616161"))
+                            HStack(spacing: 0) {
+                                Text("트래블월렛 ")
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(Color(hex: "#000000"))
+                                Text("플래티늄")
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(Color(hex: "#0079FF"))
+                                Text(" 카드")
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(Color(hex: "#000000"))
+                            }
+                        }
+                        Spacer()
+                        Image("arrow")
+                    }
+                    .padding()
+                    .frame(width: 343, height: 76)
                     .background(
                         RoundedRectangle(cornerRadius: 15)
-                            .fill(Color(hex: "#FFFFFF"))
+                            .fill(
+                                LinearGradient(
+                                    gradient: Gradient(colors: [
+                                        Color(hex: "6FE8B7"),
+                                        Color(hex: "35DAE3")
+                                    ]),
+                                    startPoint: .leading,
+                                    endPoint: .trailing
+                                )
+                            )
                     )
                     .padding(.bottom)
-                }
-                
-                // 수평 스크롤 카드
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 8) {
-                        CardView(icon: "N", subtitle: "결제부터", title: "N빵하기")
-                        CardView(icon: "remit", subtitle: "친구에게", title: "송금하기")
-                        CardView(icon: "exchange", subtitle: "외화간", title: "환전하기")
+                    
+                    // 내 지갑
+                    HStack{
+                        VStack(spacing: 45) {
+                            HStack {
+                                Text("내 지갑")
+                                    .font(.system(size: 18, weight: .semibold))
+                                Image("info")
+                                
+                                Spacer()
+                                
+                                Text("이용내역")
+                                    .font(.system(size: 15, weight: .medium))
+                                Image("arrow")
+                                    .padding(.leading, -13)
+                            }
+                            .padding(.horizontal, 5)
+                            
+                            Text("아직 충전된 외화가 없습니다.")
+                                .font(.system(size: 13))
+                                .foregroundColor(Color(hex: "#616161"))
+                            
+                            NavigationLink(destination: Charge()) {
+                                Text("충전하기")
+                                    .font(.system(size: 15, weight: .bold))
+                                    .foregroundColor(.white)
+                                    .frame(width: 302, height: 48)
+                                    .background(Color(hex: "#0BAEFF"))
+                                    .cornerRadius(12)
+                            }
+                        }
+                        .padding()
+                        .frame(width: 343, height: 228)
+                        .background(
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color(hex: "#FFFFFF"))
+                        )
                     }
-                    .padding(.horizontal, 25)
+                    .padding(.bottom)
+                    
+                    // 달러박스
+                    if isDollarBoxVisible {
+                        HStack {
+                            HStack(spacing: 20) {
+                                Image("kakaoBank")
+                                
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text("카카오뱅크 이용중이시라면")
+                                        .font(.system(size: 10))
+                                    Text("달러박스를 연동해보세요!")
+                                        .font(.system(size: 13, weight: .bold))
+                                }
+                            }
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                isDollarBoxVisible = false
+                            }) {
+                                Image("xmark")
+                            }
+                        }
+                        .padding()
+                        .frame(width: 343, height: 66)
+                        .background(
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color(hex: "#FFFFFF"))
+                        )
+                        .padding(.bottom)
+                    }
+                    
+                    // 수평 스크롤 카드
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 8) {
+                            CardView(icon: "N", subtitle: "결제부터", title: "N빵하기")
+                            CardView(icon: "remit", subtitle: "친구에게", title: "송금하기")
+                            CardView(icon: "exchange", subtitle: "외화간", title: "환전하기")
+                        }
+                        .padding(.horizontal, 25)
+                    }
+                    Spacer()
                 }
-                Spacer()
+                .background(Color(hex: "#F4F4F4"))
+                .navigationBarHidden(true)
             }
-            .background(Color(hex: "#F4F4F4"))
-            
+            .navigationBarHidden(true)
             
             .tabItem {
                 Label("페이", image: "pay")
