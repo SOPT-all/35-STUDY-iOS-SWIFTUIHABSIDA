883bf5857f05612288acd6565e7a1671bb050029
sem-git <lsmsm0327@naver.com>
2024-12-08 01:46:00 +0900

    feat: 플래티늄 카드 뷰 UI 구현

--- LeeSeminSU/LeeSeminSU/Week5 - TravelWallet/Main.swift
+++ LeeSeminSU/LeeSeminSU/Week5 - TravelWallet/Main.swift
@@ -35,6 +35,43 @@ struct Main: View {
                 }
                 .padding()
                 
+                HStack {
+                    Image("platinumCard")
+                    VStack(alignment: .leading, spacing: 5){
+                        Text("드디어 나왔다!")
+                            .font(.system(size: 13))
+                            .foregroundColor(Color(hex: "#616161"))
+                        HStack(spacing: 0) {
+                            Text("트래블월렛 ")
+                                .font(.system(size: 16, weight: .bold))
+                                .foregroundColor(Color(hex: "#000000"))
+                            Text("플래티늄")
+                                .font(.system(size: 16, weight: .bold))
+                                .foregroundColor(Color(hex: "#0079FF"))
+                            Text(" 카드")
+                                .font(.system(size: 16, weight: .bold))
+                                .foregroundColor(Color(hex: "#000000"))
+                        }
+                    }
+                    Spacer()
+                    Image("arrow")
+                }
+                .padding()
+                .frame(width: 343, height: 76)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: [
+                                    Color(hex: "6FE8B7"),
+                                    Color(hex: "35DAE3")
+                                ]),
+                                startPoint: .leading,
+                                endPoint: .trailing
+                            )
+                        )
+                )
+                
                 Spacer()
             }
             
@@ -74,4 +111,19 @@ struct Main: View {
 
 #Preview {
     Main()
+}
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >>  8) & 0xFF) / 255.0
+        let b = Double((rgb >>  0) & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
+    }
 }
