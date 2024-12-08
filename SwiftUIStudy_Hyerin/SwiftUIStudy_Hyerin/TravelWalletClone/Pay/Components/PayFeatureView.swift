e9b2269107fc9dd8c0877c20cd8ef5c527ba6de3
JoHyerin <gpfls468@gmail.com>
2024-12-08 22:22:03 +0900

    [Feat] PayFeatureView UI 구현

---
+++ SwiftUIStudy_Hyerin/SwiftUIStudy_Hyerin/TravelWalletClone/Pay/Components/PayFeatureView.swift
@@ -0,0 +1,25 @@
+//
+//  PayFeatureView.swift
+//  SwiftUIStudy_Hyerin
+//
+//  Created by 조혜린 on 12/8/24.
+//
+
+import SwiftUI
+
+struct PayFeatureView: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 7) {
+                FeatureView(iconImage: .imgN, subText: "결제부터", featureText: "N빵하기")
+                FeatureView(iconImage: .imgPaperAirplane, subText: "친구에게", featureText: "송금하기")
+                FeatureView(iconImage: .imgForex, subText: "외화간", featureText: "송금하기")
+            }
+        }
+        .scrollIndicators(.hidden)
+    }
+}
+
+#Preview {
+    PayFeatureView()
+}
