5ff8187fe1d929a4e88a9d2eadb174eba588cecb
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-12-08 22:55:46 +0900

    [Feat] NavigationView 실습 코드

---
+++ week4/week4/week4App.swift
@@ -0,0 +1,17 @@
+//
+//  week4App.swift
+//  week4
+//
+//  Created by  정지원 on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct week4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ week4/week4Tests/week4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  week4Tests.swift
+//  week4Tests
+//
+//  Created by  정지원 on 12/8/24.
+//
+
+import Testing
+@testable import week4
+
+struct week4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
